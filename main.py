0fa90c8d647cd770d60ce2a1328388cc24e1390e
Garrett Pennell <garrettpennell@gmail.com>
2012-05-01 20:49:31 -0500

    Added main.py file
    The parts of mutate that are to be user-modified will be exposed in main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+params = {
+	'maxGenerations':None,
+	'maxLength':7,
+	'minLength':3,
+	'maxValue':None,
+	'initPopSize':100,
+	'popSize':1000,
+	'mutationRate':.1,
+	'elitistSelection':False,
+	'normalizeScores':False,
+	'maxBreeders':1000,
+	'execTimePenalty':10000,
+	'execIterations':100,
+	}
+
+
